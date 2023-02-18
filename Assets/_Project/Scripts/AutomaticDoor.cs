956ccadf60ad9d590f78e2bd7b24d14d0406699d
Ivgeniay <45053991+Ivgeniay@users.noreply.github.com>
2023-02-18 22:30:10 +0400

    18.02.2023

--- Assets/_Project/Scripts/AutomaticDoor.cs
+++ Assets/_Project/Scripts/AutomaticDoor.cs
@@ -5,22 +5,15 @@ public class AutomaticDoor : MonoBehaviour
     [SerializeField] private Transform upperPoint;
     [SerializeField] private Rigidbody body;
     [SerializeField] private float force;
-    [SerializeField] private float constantForce;
 
 
     private void FixedUpdate()
     {
         var f = (upperPoint.position - body.position) * force * Time.deltaTime;
         f = new Vector3(0, f.y, 0);
-        Debug.Log(f);
-
-
 
         if (body.position != upperPoint.position)
             body.AddRelativeForce(f);
-        //body.MovePosition(upperPoint.position);
-
-        body.AddRelativeForce(Vector3.up * constantForce);
     }
 }
 
