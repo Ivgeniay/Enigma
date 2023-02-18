6e6b62badf153910282db6084f1fc39c9a84b035
Ivgeniay <45053991+Ivgeniay@users.noreply.github.com>
2023-02-19 00:25:29 +0400

    19

--- Assets/_Project/Scripts/Triggers/GhostTrigger.cs
+++ Assets/_Project/Scripts/Triggers/GhostTrigger.cs
@@ -11,15 +11,20 @@ public class GhostTrigger : MonoBehaviour
     [SerializeField] private float minPosY;
     [SerializeField] private float maxPosY;
 
+    [SerializeField] private Vector3 SpawnPoint;
+
     private bool isActive = true;
 
     private void OnTriggerEnter(Collider other)
     {
         var player = other.GetComponent<Player>();
         if (player && isActive) {
+            if (SpawnPoint == Vector3.zero)
+                SpawnPoint = transform.position;
+
             isActive = false;
             chandelierFalling.isActive = true;
-            var ghost = Instantiate(GhostPrefab, transform.position, Quaternion.identity, null);
+            var ghost = Instantiate(GhostPrefab, SpawnPoint, Quaternion.identity, null);
             ghost.SetBorder(minPosX, maxPosX, minPosY, maxPosY);
         }
     }
